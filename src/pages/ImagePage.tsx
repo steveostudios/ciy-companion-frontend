76ecded24504c042f001a6e73ba153baa4634d26
steveostudios <steveostudios@gmail.com>
2023-12-05 15:36:52 -0500

    fix image plugin

--- src/pages/ImagePage.tsx
+++ src/pages/ImagePage.tsx
@@ -1,25 +1,55 @@
 import styled from "@emotion/styled";
 import {
+  ReactZoomPanPinchRef,
   TransformComponent,
   TransformWrapper,
-} from "@pronestor/react-zoom-pan-pinch";
+} from "react-zoom-pan-pinch";
 
 import { ImagePage as PageProps } from "../helpers/types";
+import React, { useRef } from "react";
 
 export const ImagePage: React.FC<PageProps> = (props) => {
+  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+
+  // const zoomToImage = () => {
+  //   if (transformComponentRef.current) {
+  //     const { zoomToElement } = transformComponentRef.current;
+  //     zoomToElement("imgExample");
+  //   }
+  // };
+
   return (
     <Container>
       {props.data.image && (
         <TransformWrapper
-          maxScale={15}
-          minScale={0.15}
-          centerOnInit={true}
-          centerZoomedOut={true}
-          initialScale={0.15}
+          ref={transformComponentRef}
+          // centerOnInit={true}
+          // centerZoomedOut={true}
+          initialScale={1}
+          minScale={0.5}
+          limitToBounds={true}
+          // maxScale={15}
+          // minScale={0.15}
+          // centerOnInit={true}
+          // centerZoomedOut={true}
+          // initialScale={0.5}
         >
-          <TransformComponent>
-            <img src={props.data.image} alt={props.data.title} />
-          </TransformComponent>
+          {(utils) => (
+            <>
+              <Controls>
+                <Control onClick={() => utils.zoomIn()}>+</Control>
+                <Control onClick={() => utils.zoomOut()}>-</Control>
+                <Control onClick={() => utils.resetTransform()}>x</Control>
+              </Controls>
+              <TransformComponent>
+                <img
+                  src={props.data.image}
+                  alt={props.data.title}
+                  style={{ width: "100%" }}
+                />
+              </TransformComponent>
+            </>
+          )}
         </TransformWrapper>
       )}
       {!props.data.image && (
@@ -30,5 +60,43 @@ export const ImagePage: React.FC<PageProps> = (props) => {
 };
 
 const Container = styled("div")({
+  backgroundColor: "#eee",
+  height: "100%",
+  overflow: "hidden",
+  ".react-transform-wrapper": {
+    width: "calc(100% -2rem)",
+    height: "calc(100% - 4rem)",
+  },
+  "> *": {},
+});
+
+const Controls = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "1rem",
+  padding: "1rem",
   width: "100%",
+  height: "4rem",
+  backgroundColor: "rgba(0,0,0,0.1)",
+});
+
+const Control = styled("button")({
+  backgroundColor: "transparent",
+  width: "4rem",
+  height: "4rem",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "1px solid black",
+  color: "black",
+  cursor: "pointer",
+  fontSize: "2rem",
+  fontWeight: "bold",
+  "&:focus": {
+    outline: "none",
+  },
+  "&:hover": {
+    backgroundColor: "rgba(0,0,0,0.1)",
+  },
 });
